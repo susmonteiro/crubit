ccc1dd793b8ea09ccc2ab397b97a27ec363f0ad6
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2023-03-21 16:21:58 +0000

    Change tmp test

--- lifetime_analysis/test/tmp.cc
+++ lifetime_analysis/test/tmp.cc
@@ -1,13 +1,10 @@
 // Part of the Crubit project, under the Apache License v2.0 with LLVM
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
 // Tests for basic functionality.
-
 #include "lifetime_analysis/test/lifetime_analysis_test.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-
 namespace clang {
 namespace tidy {
 namespace lifetimes {
@@ -15,13 +12,15 @@ namespace {
 
 // ! writing "DISABLED_" in the beginning of the name of the test disables it
 
-TEST_F(LifetimeAnnotationsTest, LifetimeAnnotation_Simple) {
-  EXPECT_THAT(GetNamedLifetimeAnnotations(WithLifetimeMacros(R"(
-        [[clang::annotate("lifetimes", "a -> a")]]
-        int* f1(int*);
-        int* $a f2(int* $a);
-  )")),
-              IsOkAndHolds(LifetimesAre({{"f1", "a -> a"}, {"f2", "a -> a"}})));
+TEST_F(LifetimeAnalysisTest, TwoFunctions) {
+  EXPECT_THAT(GetLifetimes(R"(
+    int* fn(int* x, int* y, int* z) {
+      y = x;
+      y = z;
+      return y;
+    }
+  )"),
+              LifetimesAre({{"fn", "a, b, c -> c"}}));
 }
 
 } // namespace
